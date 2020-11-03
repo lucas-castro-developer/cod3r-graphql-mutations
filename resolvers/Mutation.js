05314bb49b00b88e2410e1972d41a91f381ed426
Lucas Castro <lucas.sc98@hotmail.com>
2020-11-03 19:43:53 -0300

    Mutation to delete user

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,21 +1,27 @@
-const { usuarios, proximoId } = require('../data/db')
+const { usuarios, proximoId } = require("../data/db");
 
 module.exports = {
-    novoUsuario(_, args) {
-        const emailExistente = usuarios.some(u => u.email === args.email)
-        
-        if(emailExistente) {
-            throw new Error('E-mail cadastrado!')
-        }
+  novoUsuario(_, args) {
+    const emailExistente = usuarios.some((u) => u.email === args.email);
 
-        const novo = {
-            id: proximoId(),
-            ...args,
-            perfil_id: 1,
-            status: 'ATIVO'
-        }
-
-        usuarios.push(novo)
-        return novo
+    if (emailExistente) {
+      throw new Error("E-mail cadastrado!");
     }
-}
+
+    const novo = {
+      id: proximoId(),
+      ...args,
+      perfil_id: 1,
+      status: "ATIVO",
+    };
+
+    usuarios.push(novo);
+    return novo;
+  },
+  excluirUsuario(_, { id }) {
+    const i = usuarios.findIndex((u) => u.id === id);
+    if (i < 0) return null;
+    const excluidos = usuarios.splice(i, 1);
+    return excluidos ? excluidos[0] : null;
+  },
+};
