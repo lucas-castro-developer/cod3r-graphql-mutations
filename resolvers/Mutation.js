7b8723dec1885ca5acd89fe95a4806359d034c7b
Lucas Castro <lucas.sc98@hotmail.com>
2020-11-06 20:26:10 -0300

    Using input #02

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,5 +1,16 @@
 const { usuarios, proximoId } = require("../data/db");
 
+function indiceUsuario(filtro) {
+  if (!filtro) return -1;
+  const { id, email } = filtro;
+  if (id) {
+    return usuarios.findIndex((u) => u.id === id);
+  } else if (email) {
+    return usuarios.findIndex((u) => u.email === email);
+  }
+  return -1
+}
+
 module.exports = {
   novoUsuario(_, { dados }) {
     const emailExistente = usuarios.some((u) => u.email === dados.email);
@@ -18,23 +29,22 @@ module.exports = {
     usuarios.push(novo);
     return novo;
   },
-  excluirUsuario(_, { id }) {
-    const i = usuarios.findIndex((u) => u.id === id);
+  excluirUsuario(_, { filtro }) {
+    const i = indiceUsuario(filtro);
     if (i < 0) return null;
     const excluidos = usuarios.splice(i, 1);
     return excluidos ? excluidos[0] : null;
   },
   alterarUsuario(_, args) {
-    const i = usuarios
-      .findIndex(u => u.id === args.id)
-    if(i < 0) return null
+    const i = usuarios.findIndex((u) => u.id === args.id);
+    if (i < 0) return null;
 
     const usuario = {
       ...usuarios[i],
-      ...args
-    }
+      ...args,
+    };
 
-    usuarios.splice(i, 1, usuario)
-    return usuario
-  }
+    usuarios.splice(i, 1, usuario);
+    return usuario;
+  },
 };
